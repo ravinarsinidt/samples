b02b8476fb32d89807e1edca514a3f64ea175b28
Ravi Narsini <ravi.narsi@ibridgetechsoft.com>
2023-06-27 12:11:08 +0530

    Added word reverse program

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -6,7 +6,69 @@ namespace ReverseString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = null;
+            string[] mywords = Split(input);
+            string revstring = ReverseEachWord(mywords);
+            Console.WriteLine("[" + revstring.Trim() + "]");
+        }
+
+        private static string ReverseEachWord(string[] mywords)
+        {
+            string reversedString = string.Empty;
+            for(int i = 0; i < mywords.Length; i++)
+            {
+                string word = mywords[i];
+                reversedString = reversedString + ReverseWord(word);
+            }
+
+            return reversedString;
+        }
+
+        private static string ReverseWord(string word)
+        {
+            string rstring = string.Empty;
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                rstring = rstring + word[i];
+            }
+
+            return rstring + " ";
+        }
+
+        private static string[] Split(string input)
+        {
+            int noOfWords = GetNumberOfWords(input);
+            string[] mywords = new string[noOfWords];
+            string word = string.Empty;
+            int wordCount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    word = word + input[i];
+                }
+                else
+                {
+                    mywords[wordCount] = word;
+                    wordCount++;
+                    word = string.Empty;
+                }
+            }
+            mywords[wordCount] = word;
+            return mywords;
+        }
+
+        private static int GetNumberOfWords(string input)
+        {
+            int spaces = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    spaces++;
+                }
+            }
+            return spaces + 1;
         }
     }
 }
