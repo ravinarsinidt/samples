0600d39191a7cb82d0a0be4cbf700f6f7bbf7120
Ravi Narsini <ravi.narsi@ibridgetechsoft.com>
2023-08-26 12:18:20 +0530

    Changed Result.cs

--- AudreeBankApi/AudreeBankApi/Result.cs
+++ AudreeBankApi/AudreeBankApi/Result.cs
@@ -7,5 +7,6 @@ namespace AudreeBankApi
         public int Code { get; set; }
         public List<string> Errors  { get; set; }
         public object Data { get; set; }
+        public int MyProperty { get; set; }
     }
 }
